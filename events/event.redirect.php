35e51074610786f5cd5cd0f60a2522e97741e9d9
michael-e <info@michael-eichelsdoerfer.de>
2013-09-01 13:35:26 +0200

    PSR-2 formatting.

--- events/event.redirect.php
+++ events/event.redirect.php
@@ -1,49 +1,48 @@
 <?php
 
-	if(!defined('__IN_SYMPHONY__')) die('<h2>Error</h2><p>You cannot directly access this file</p>');
+    if(!defined('__IN_SYMPHONY__')) die('<h2>Error</h2><p>You cannot directly access this file</p>');
 
-	require_once(TOOLKIT . '/class.event.php');
+    require_once(TOOLKIT . '/class.event.php');
 
-	Class eventRedirect extends Event{
+    Class eventRedirect extends Event{
 
-		public static function about(){
+        public static function about()
+        {
+            $description = new XMLElement('p', 'This event redirects the users to the first subpage attached to the page');
 
-			$description = new XMLElement('p', 'This event redirects the users to the first subpage attached to the page');
+            return array(
+                         'name' => 'Subpage Redirect',
+                         'author' => array('name' => 'Huib Keemink',
+                                           'website' => 'http://www.creativedutchmen.com',
+                                           'email' => 'huib@creativedutchmen.com'),
+                         'version' => '1.0',
+                         'release-date' => '2008-11-10',
+                         'trigger-condition' => '');
+        }
 
-			return array(
-						 'name' => 'Subpage Redirect',
-						 'author' => array('name' => 'Huib Keemink',
-										   'website' => 'http://www.creativedutchmen.com',
-										   'email' => 'huib@creativedutchmen.com'),
-						 'version' => '1.0',
-						 'release-date' => '2008-11-10',
-						 'trigger-condition' => '');
-		}
+        public function load()
+        {
+            return $this->__trigger();
+        }
 
-		public function load(){
-			return $this->__trigger();
-		}
+        public static function documentation()
+        {
+            return 'Redirects the user to the first subpage associated with the page';
+        }
 
-		public static function documentation(){
-			return 'Redirects the user to the first subpage associated with the page';
-		}
-
-		protected function __trigger(){
-			if($this->_env['param']['parent-path'] == '/'){
-				$path = $this->_env['param']['current-page'];
-			}
-			else{
-				$path = substr($this->_env['param']['parent-path'].'/'.$this->_env['param']['current-page'],1);
-			}
+        protected function __trigger()
+        {
+            if ($this->_env['param']['parent-path'] == '/') {
+                $path = $this->_env['param']['current-page'];
+            } else {
+                $path = substr($this->_env['param']['parent-path'].'/'.$this->_env['param']['current-page'],1);
+            }
             $row = $this->_Parent->Database->fetchRow(0, "SELECT * from tbl_pages where path = '".$path."' ORDER BY sortorder ASC");
-			if(empty($row)){
-				return false;
-			}
-			else{
-				header ('Location: '.$this->_env['param']['root'].'/'.$path.'/'.$row['handle'] . '/');
-				die();
-			}
-		}
-	}
-
-?>
+            if (empty($row)) {
+                return false;
+            } else {
+                header ('Location: '.$this->_env['param']['root'].'/'.$path.'/'.$row['handle'] . '/');
+                die();
+            }
+        }
+    }
